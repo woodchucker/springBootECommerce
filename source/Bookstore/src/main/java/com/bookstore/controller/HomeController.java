26f05efcef043bb55e5c06d83f0dbfc7fa6703ac
woodchucker <ditrafabri@gmail.com>
2020-02-21 10:23:27 +0100

    Secur, Repo, HCtrler modelTo myAcc, loginhlink

--- source/Bookstore/src/main/java/com/bookstore/controller/HomeController.java
+++ source/Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -1,18 +1,37 @@
 package com.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
-
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
+		return "myAccount";
+	}
+	
 	@RequestMapping("/myAccount")
 	public String myAccount() {
+		return "myAccount";
+	}
+	
+	
+	@RequestMapping("/forgetPassword")
+	public String forgetPassword(Model model) {
+		model.addAttribute("classActiveForgetPassword", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/newUser")
+	public String newUser(Model model) {
+		model.addAttribute("classActiveNewUser", true);
 		return "myAccount";
 	}
 }
